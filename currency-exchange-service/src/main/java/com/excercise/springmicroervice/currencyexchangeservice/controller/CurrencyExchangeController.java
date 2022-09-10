97ea1aefd77d080db784e201d62f5e3c060dd639
Bishal Thapa <dev.bith@gmail.com>
2022-09-10 12:08:56 +0100

    Added CurrencyExchange Controller
    - CurrencyExchangeController is simplle REST controller
    - Takeway from this commit:
      P: How to find LoadBalancing is working properly in microservice?
         How to find multiple instance of currency-exchange-service is
         is serving the request in microservice architecture?
    
      S: We created a property environment inside CurrencyExchange DTO
      to track the port of currency-exchange-service that returned
      the value. This way we can identify which instance of currency-
      exchange-service is responding back.

---
+++ currency-exchange-service/src/main/java/com/excercise/springmicroervice/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -0,0 +1,25 @@
+package com.excercise.springmicroervice.currencyexchangeservice.controller;
+
+import com.excercise.springmicroervice.currencyexchangeservice.bean.CurrencyExchange;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+public class CurrencyExchangeController {
+
+    @Autowired
+    private Environment environment;
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyExchange retrieveExchange(@PathVariable String from, @PathVariable String to) {
+        final String port = environment.getProperty("local.server.port");
+        final CurrencyExchange currencyExchange = new CurrencyExchange(100L, from, to, BigDecimal.valueOf(50));
+        currencyExchange.setEnvironment(port);
+        return currencyExchange;
+    }
+}
