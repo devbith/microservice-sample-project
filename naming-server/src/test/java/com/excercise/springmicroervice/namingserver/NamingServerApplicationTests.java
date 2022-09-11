56a633b7cb17f00584694e202a95e089d76a8e6f
Bishal Thapa <dev.bith@gmail.com>
2022-09-11 23:32:06 +0100

    Init naming-server project

---
+++ naming-server/src/test/java/com/excercise/springmicroervice/namingserver/NamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excercise.springmicroervice.namingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
