e53debfe52ed830e2af55736dad317ef00cfbdc5
Bishal Thapa <dev.bith@gmail.com>
2022-09-14 22:48:53 +0100

    Added api gateway configuration

---
+++ api-gateway/src/main/java/com/excercise/springmicroervice/apigateway/config/ApiGatewayConfiguration.java
@@ -0,0 +1,16 @@
+package com.excercise.springmicroervice.apigateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfiguration {
+
+  @Bean
+  public RouteLocator gatewayRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
+    return routeLocatorBuilder.routes()
+        .route(p -> p.path("/get").uri("http://httpbin.org")).build();
+  }
+}
