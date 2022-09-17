196692e81e61941302a8e3f6d8be9769776a8ac3
Bishal Thapa <dev.bith@gmail.com>
2022-09-17 20:27:14 +0100

    Integrated Resilience4j fault tolerance framework
    - Added resilience4j dependecny
    - Integrated Retry feature of resilience4j
      • Created custom retry config (sample-api-retry) in application.yml
      • Created fallback method for REST call

---
+++ currency-exchange-service/src/main/java/com/excercise/springmicroervice/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -0,0 +1,27 @@
+package com.excercise.springmicroervice.currencyexchangeservice.controller;
+
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class CircuitBreakerController {
+
+    private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+
+    @GetMapping("/sample-api")
+    @Retry(name = "sample-api-retry", fallbackMethod = "hardCodedResponse")
+    public String sampleApi() {
+        logger.info("Sample API call received");
+        ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-duppy-url", String.class);
+        return forEntity.getBody();
+    }
+
+    public String hardCodedResponse(Exception e) {
+        return "fallback-response";
+    }
+}
