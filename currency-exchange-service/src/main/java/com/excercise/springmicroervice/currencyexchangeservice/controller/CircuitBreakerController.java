1a3c3cd6211a2ee011309683e41fe88703f63569
Bishal Thapa <dev.bith@gmail.com>
2022-09-17 23:08:09 +0100

    Integrated @CircuitBreaker of resilience4j
    - https://resilience4j.readme.io/docs/circuitbreaker

--- currency-exchange-service/src/main/java/com/excercise/springmicroervice/currencyexchangeservice/controller/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/excercise/springmicroervice/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -1,5 +1,6 @@
 package com.excercise.springmicroervice.currencyexchangeservice.controller;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,7 +15,8 @@ public class CircuitBreakerController {
     private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
 
     @GetMapping("/sample-api")
-    @Retry(name = "sample-api-retry", fallbackMethod = "hardCodedResponse")
+    //@Retry(name = "sample-api-retry", fallbackMethod = "hardCodedResponse")
+    @CircuitBreaker(name = "sample-api-retry", fallbackMethod = "hardCodedResponse")
     public String sampleApi() {
         logger.info("Sample API call received");
         ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-duppy-url", String.class);
