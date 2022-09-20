238ac559f29b03410b7f3a87144f921ff98519cd
Bishal Thapa <dev.bith@gmail.com>
2022-09-20 09:36:09 +0100

    Configured sleuth with zipkin

--- currency-exchange-service/src/main/java/com/excercise/springmicroervice/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/excercise/springmicroervice/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.excercise.springmicroervice.currencyexchangeservice.controller;
 
 import com.excercise.springmicroervice.currencyexchangeservice.model.CurrencyExchange;
 import com.excercise.springmicroervice.currencyexchangeservice.service.CurrencyExchangeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +15,8 @@ import java.util.Optional;
 @RestController
 public class CurrencyExchangeController {
 
+    private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+
     @Autowired
     private Environment environment;
 
@@ -21,6 +25,7 @@ public class CurrencyExchangeController {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchange(@PathVariable String from, @PathVariable String to) {
+        logger.info("retrieve value from {} to {}", from, to);
         final String port = environment.getProperty("local.server.port");
         Optional<CurrencyExchange> currencyExchangeOptional = currencyExchangeService.findByFromAndTo(from, to);
         if (currencyExchangeOptional.isPresent()) {
